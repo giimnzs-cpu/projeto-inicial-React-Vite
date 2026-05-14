002437e09912fe9a278bfdf99f6ec55bde76a85e
giimnzs-cpu <s.giovannamenezes@gmail.com>
2026-05-14 10:38:39 -0300

    Modificações do Form

--- src/components/FormularioCadastro.jsx
+++ src/components/FormularioCadastro.jsx
@@ -4,39 +4,76 @@ import Contador from './Contador';
 import { useState } from 'react';
 
 function FormularioCadastro() {
-    const [nome, setNome] = useState('')
-    const [email, setEmail] = useState('')
-    const [telefone, setTelefone] = useState('')
+    // const [nome, setNome] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [telefone, setTelefone] = useState('')
     const [number, setNumber] = useState('')
     const [erro, setErro] = useState('')
     const [sucesso, setSucesso] = useState(false)
+    const [user, setUser] = useState({ nome: "", email: "", telefone: "", segundoTelefone: "" })
 
     const handleSubmit = (e) => {
-     e.preventdefault() // Não deixa carregar a página.
+        e.preventDefault() // Não deixa carregar a página.
 
-    if (nome.trim() === ""){
-        setErro('O campo desse nome está sem nada, preencha!')
-        console.log(erro)
-        return
-}
+        if (user.nome.trim() === "") {
+            setErro('O campo desse nome está sem nada, preencha!')
+            console.log(erro)
+            return
+        }
 
-  setErro('')
-  setSucesso(true)
-    console.log({nome, email, telefone})// Envio para o banco
-    setSucesso(false)
-}
+        if (user.telefone.length != 11) {
 
-    return(
-     <form onSubmit={handleSubmit}>
+            setErro("O campo necessita de 11 dígitos")
+            console.log({ erro })
+            return
+        }
+    
 
-            {erro && <p style={{color: "red"}}>{erro}</p>}
-            {sucesso && <p style={{color: "green"}}>{sucesso}</p>}
-            <InputField label="Nome:" type="text" name="nome" placeholder="Digite seu nome..." value={nome} onChange={(e) => setNome(e.target.value)} />
-            <InputField label="Email:" type= "email" name="email" placeholder="email@empresa.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <InputField label="Idade:" type="number" name="number" placeholder="18 anos" value={number} onChange={(e) => setNumber(e.target.value)} />
-            <InputField label="Telefone:" type="text" name="Telefone" placeholder="(79) 99999-9999" value={telefone} onChange={(e) => setTelefone(e.target.value)} /> 
-            <BotaoEnviar texto="Cadastrar" />
-     </form>
-    )
+        setErro('')
+        setSucesso(true)
+        console.log(user)// Envio para o banco
     }
+    return (
+
+        <form onSubmit={handleSubmit}>
+
+            {erro && <p style={{ color: "red" }}>{erro}</p>}
+            {sucesso && <p style={{ color: "green" }}>Sucesso!</p>}
+            <InputField label="Nome:" type="text" name="nome"
+                placeholder="Digite seu nome..."
+                value={user.nome}
+                onChange={(e) => setUser((dados) => ({
+                    ...dados,
+                    nome: e.target.value,
+                }))} />
+
+            <InputField label="Email:" type="email" name="email"
+                placeholder="email@empresa.com"
+                value={user.email}
+                onChange={(e) => setUser((dados) => ({
+                    ...dados,
+                    email: e.target.value
+                }))} />
+
+            <InputField label="Idade:" type="number" name="number"
+                placeholder="18 anos"
+                value={user.number}
+                onChange={(e) => setNumber((dados) => ({
+                    ...dados,
+                    number: e.target.value
+                }))} />
+
+            <InputField label="Telefone:" type="text" name="Telefone"
+                placeholder="(79) 99999-9999"
+                value={user.telefone}
+                onChange={(e) => setUser((dados) => ({
+                    ...dados,
+                    telefone: e.target.value
+                }))} />
+
+            <BotaoEnviar texto="Cadastrar-se" />
+        </form>
+
+    )
+}
 export default FormularioCadastro;
